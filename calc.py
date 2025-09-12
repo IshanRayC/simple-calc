3e8d4a6ecd9fc5ea2d59a658d5fb12d6f582fa82
Ishan Ray <ransanishois@gmail.com>
2025-09-12 23:37:14 +0530

    Initial commit: add calculator script

---
+++ calc.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ValueError("Division by zero")
+    return a / b
+
+if __name__ == "__main__":
+    print("2 + 3 =", add(2,3))
+    print("5 - 2 =", sub(5,2))
+    print("3 * 4 =", mul(3,4))
+    print("8 / 2 =", div(8,2))
