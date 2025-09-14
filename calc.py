883e0c9d9b3e407ec146e8af61b1b5cf34168639
Ishan Ray <ransanishois@gmail.com>
2025-09-14 11:55:17 +0530

    modified to operate on n numbers instead of just two

--- calc.py
+++ calc.py
@@ -1,64 +1,79 @@
-def add(a, b):
-    return a + b
+def add(numbers):
+    return sum(numbers)
 
-def sub(a, b):
-    return a - b
+def sub(numbers):
+    result = numbers[0]
+    for num in numbers[1:]:
+        result -= num
+    return result
 
-def mul(a, b):
-    return a * b
+def mul(numbers):
+    result = 1
+    for num in numbers:
+        result *= num
+    return result
 
-def div(a, b):
-    if b == 0:
-        raise ValueError("Division by zero")
-    return a / b
-def pow(a,b):
-    return a**b
+def div(numbers):
+    result = numbers[0]
+    for num in numbers[1:]:
+        if num == 0:
+            raise ValueError("Division by zero")
+        result /= num
+    return result
 
-def pow(a,b):
-    return a**b
+def pow(numbers):
+    result = numbers[0]
+    for num in numbers[1:]:
+        result **= num
+    return result
 
-def mod(a,b):
-    return a%b
+def mod(numbers):
+    result = numbers[0]
+    for num in numbers[1:]:
+        result %= num
+    return result
+
 
 if __name__ == "__main__":
-    print("#Example of the calculator")
-    print("2 + 3 =", add(2,3))
-    print("5 - 2 =", sub(5,2))
-    print("3 * 4 =", mul(3,4))
-    print("8 / 2 =", div(8,2))
-    print("9 ^ 3 =", pow(9,3))
-    print("10 % 3 =", mod(10,3))
+    print("Welcome to the n-number calculator!")
+    print("Choose the operation you want to perform:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exponentiation")
+    print("6. Modulus")
+    print("7. Exit")
 
-print("for performing calculations kindly enter the number of the function")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
-print("5. Exponentiation")
-print("6. Modulus")
-print("7. Exit")
+    while True:
+        choice = input("Enter choice (1/2/3/4/5/6/7): ")
 
-choice=input("Enter choice(1/2/3/4/5/6): ")
-while choice in ['1','2','3','4','5','6']:
-    print("Welcome to the simple calculator!")
-    num1=float(input("Enter first number: "))
-    num2=float(input("Enter second number: "))
-    if choice=='1':
-        print(num1,"+",num2,"=",add(num1,num2))
-    elif choice=='2':
-        print(num1,"-",num2,"=",sub(num1,num2))
-    elif choice=='3':
-        print(num1,"*",num2,"=",mul(num1,num2))
-    elif choice=='4':
+        if choice == '7':
+            print("Exiting the calculator. Goodbye!")
+            break
+
+        n = int(input("Enter how many numbers you want to operate on: "))
+        numbers = []
+        for i in range(n):
+            numbers.append(float(input(f"Enter number {i+1}: ")))
+
         try:
-            print(num1,"/",num2,"=",div(num1,num2))
+            if choice == '1':
+                print("Result =", add(numbers))
+            elif choice == '2':
+                print("Result =", sub(numbers))
+            elif choice == '3':
+                print("Result =", mul(numbers))
+            elif choice == '4':
+                print("Result =", div(numbers))
+            elif choice == '5':
+                print("Result =", pow(numbers))
+            elif choice == '6':
+                print("Result =", mod(numbers))
+            else:
+                print("Invalid choice. Try again!")
         except ValueError as e:
-            print(e)
-    elif choice=='5':
-        print(num1,"^",num2,"=",pow(num1,num2))
-    elif choice=='6':
-        print(num1,"%",num2,"=",mod(num1,num2))
-    choice=input("Enter choice(1/2/3/4/5/6/7): ")
-if choice=='7':
-    print("Exiting the calculator. Goodbye!")
+            print(e,"Try again!,Can't divide by zero")
+            
+
 
